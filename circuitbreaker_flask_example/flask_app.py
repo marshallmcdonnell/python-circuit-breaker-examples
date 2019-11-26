4e18b60037759d91274da6550a7c9ad15695ad25
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-11-26 16:31:36 -0500

    Added /shutdown endpoint to flask app

--- circuitbreaker_flask_example/flask_app.py
+++ circuitbreaker_flask_example/flask_app.py
@@ -1,15 +1,22 @@
+import flask
 
+app = flask.Flask(__name__)
 
-from flask import Flask
-
-
-app = Flask(__name__)
+def shutdown_server():
+    func = flask.request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 
 @app.route('/greetings')
 def hello():
     return 'Hello, World!'
 
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 if __name__ == '__main__':
     app.run()
