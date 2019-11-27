36194caaa1986ef81faaef1dd181790547dc8650
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-11-27 10:15:39 -0500

    Simplified client script name

--- circuitbreaker_flask_example/client.py
+++ circuitbreaker_flask_example/client.py
@@ -4,7 +4,7 @@ from circuitbreaker_flask_example.mock_server import app
 
 def get_greeting(url, port):
     response = requests.get("http://{}:{}/".format(url, port))
-    print(response.text)
+    return response.text
 
 if __name__ == "__main__":
     url = "localhost"
