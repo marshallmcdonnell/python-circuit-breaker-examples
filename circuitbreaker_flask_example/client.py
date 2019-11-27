8450b580667b570fb785e0d3d9635dc051cfd526
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-11-27 12:10:20 -0500

    Added working example of using circuitbreaker (can't suppress Flask logs...)

--- circuitbreaker_flask_example/client.py
+++ circuitbreaker_flask_example/client.py
@@ -1,16 +1,34 @@
 import requests
+import time
+from circuitbreaker import circuit, CircuitBreakerMonitor
 
-from circuitbreaker_flask_example.mock_server import app
+from circuitbreaker_flask_example import mock_server
 
+@circuit(failure_threshold=3, recovery_timeout=6)
 def get_greeting(url, port):
     ''' Get the main index page response '''
     response = requests.get("http://{}:{}/".format(url, port))
     return response.text
 
-if __name__ == "__main__":
-    url = "localhost"
-    port = 5000
-    with app.run(url, port):
-        get_greeting(url, port)
+def greetings_test(iterations=10, delay=1, url="localhost", port=5000):
+    for i in range(iterations): 
+        try: 
+            get_greeting(url, port) 
+        except: 
+            pass
+ 
+        print_summary()
+        time.sleep(delay) 
+ 
+def print_summary():
+    for x in CircuitBreakerMonitor.get_circuits(): 
+        msg = "{} circuit state: {}. Time till open: {}"
+        print(msg.format(x.name, x.state, x.open_remaining))
 
-    get_greeting(url, port)
+if __name__ == "__main__":
+    print("Server is turned OFF...") 
+    greetings_test()
+         
+    print("Server is turning ON...") 
+    with mock_server.app.run("localhost", 5000):
+        greetings_test()
