81baa401890b4c839008f931c2ae31a92cf94bb7
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-11-27 09:45:58 -0500

    Switched circuitbreaker client over to use mock server

--- circuitbreaker_flask_example/circuitbreaker_client.py
+++ circuitbreaker_flask_example/circuitbreaker_client.py
@@ -1,4 +1,15 @@
 import requests
-url = 'http://localhost:5000/greetings'
-response = requests.get(url)
-print(response)
+
+from circuitbreaker_flask_example.mock_server import app
+
+def get_greeting(url, port):
+    response = requests.get("http://{}:{}/".format(url, port))
+    print(response.text)
+
+if __name__ == "__main__":
+    url = "localhost"
+    port = 5000
+    with app.run(url, port):
+        get_greeting(url, port)
+
+    get_greeting(url, port)
